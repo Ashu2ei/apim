e0e7b0ebbfbe77cbf829c19ca66aa7a79d55a86e
Ashutosh Mishra1 <7532@xceedance.com>
2024-09-12 11:59:06 +0530

    hello

--- app.py
+++ app.py
@@ -74,6 +74,11 @@ def api2():
     except Exception as e:
         return str(e), 500
     
+
+@app.route('/')
+def home():
+    return "Welcome to the Flask App!"
+
 @app.route("/api3")
 def api3():
     return jsonify(large_json3)
@@ -142,3 +147,48 @@ if __name__ == "__main__":
 
 ##################
 
+# <policies>
+#     <inbound>
+#         <base />
+#         <!-- Extract the Bearer token from the Authorization header -->
+#         <set-variable name="Bearer Token" value="@(context.Request.Headers.GetValueOrDefault("Authorization", "").Substring(7))" />
+#         <!-- Retrieve the secret from Key Vault -->
+#         <set-variable name="KeyVaultSecret" value="12345" />
+#         <!-- Check if both the Bearer token and the Key Vault secret are strings and have the same length -->
+#         <choose>
+#             <when condition="@(context.Variables["Bearer Token"] is string && context.Variables["KeyVaultSecret"] is string && ((string)context.Variables["Bearer Token"]).Length == ((string)context.Variables["KeyVaultSecret"]).Length)">
+#                 <!-- Compare the Bearer token with the Key Vault secret -->
+#                 <choose>
+#                     <!-- <when condition="@(context.Variables["Bearer Token"] == context.Variables["KeyVaultSecret"])">
+#                         <return-response>
+#                             <set-status code="200" reason="OK" />
+#                             <set-body>Bearer token matches the secret.</set-body>
+#                         </return-response>
+#                     </when> -->
+#                     <when condition="@($"{context.Variables["Bearer Token"]}" == $"{context.Variables["KeyVaultSecret"]}")" />
+#                     <otherwise>
+#                         <return-response>
+#                             <set-status code="401" reason="Unauthorized" />
+#                             <set-body>@($"Bearer token does not matches the secret. Bearer Token: {context.Variables["Bearer Token"]}, Key Vault Secret: {context.Variables["KeyVaultSecret"]}/5")</set-body>
+#                         </return-response>
+#                     </otherwise>
+#                 </choose>
+#             </when>
+#             <otherwise>
+#                 <return-response>
+#                     <set-status code="400" reason="Bad Request" />
+#                     <set-body>@($"Bearer token does not matches the secret. Bearer Token: {context.Variables["Bearer Token"]}, Key Vault Secret: {context.Variables["KeyVaultSecret"]}")</set-body>
+#                 </return-response>
+#             </otherwise>
+#         </choose>
+#     </inbound>
+#     <backend>
+#         <base />
+#     </backend>
+#     <outbound>
+#         <base />
+#     </outbound>
+#     <on-error>
+#         <base />
+#     </on-error>
+# </policies>
