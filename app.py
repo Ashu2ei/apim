e13af67fd57ff88b6c8ffbd8d85d90888b7459eb
Ashutosh Mishra1 <7532@xceedance.com>
2024-09-24 12:46:38 +0530

    fdsvjb

--- app.py
+++ app.py
@@ -156,7 +156,7 @@ import logging
 from flask import Flask
 app = Flask(__name__)
 if __name__ != '__main__':
-    gunicorn_logger = logging.getLogger('gunicorn.info')
+    gunicorn_logger = logging.getLogger('gunicorn.error')
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
 else:
